0730cd79fd850a0458db844caf2cdf873c6eebe4
RandyTembe <randynael.tembesaoua@ynov.com>
2025-09-17 14:17:58 +0200

    merchant

---
+++ Money/merchant.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+)
+
+type PotionDeVie struct {
+	Name     string
+	Quantite int
+}
+
+type MarchandNoir struct {
+	Name      string
+	Inventory []PotionDeVie
+	Usable    bool
+}
+
+func accessInventory(p *MarchandNoir) {
+	fmt.Println("\n=== Inventaire du Marché Noir ===")
+
+	if len(p.Inventory) == 0 {
+		fmt.Println("\n=== Ton inventaire est vide ===")
+		return
+	}
+
+	for _, potion := range p.Inventory {
+		fmt.Printf("Potion : %s | Quantité : %d\n", potion.Name, potion.Quantite)
+	}
+}
+
+func main() {
+	marchand := MarchandNoir{
+		Name:      "Marchand Test",
+		Inventory: []PotionDeVie{},
+		Usable:    true,
+	}
+
+	accessInventory(&marchand)
+
+	marchand.Inventory = append(marchand.Inventory,
+		PotionDeVie{"Potion de Vie", 30},
+	)
+
+	accessInventory(&marchand)
+}
